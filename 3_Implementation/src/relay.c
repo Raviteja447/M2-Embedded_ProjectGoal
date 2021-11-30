3ab2628d197fee90b95370a018f31e577adf6ea8
Raviteja447 <94214892+Raviteja447@users.noreply.github.com>
2021-11-30 15:54:09 +0530

    Add files via upload

---
+++ 3_Implementation/src/relay.c
@@ -0,0 +1,8 @@
+#include <avr/io.h>
+#include"inc/relay.h"
+
+void relayInit(void)
+{
+	DDRB |= (1<<1); //set pinB1 as output
+	PORTB &= ~(1<<1); //set portB1 low
+}
---
+++ 3_Implementation/src/switch.c
@@ -0,0 +1,13 @@
+#include"inc/switch.h"
+#include<avr/io.h>
+
+void floatSwitchInit(void)
+{
+	/* top sensor */
+	DDRD &= ~(1<<0);	//set pinD0 as input
+	PORTD |= (1<<0); //use pull-up resistor on pinD0
+	
+	/* bottom sensor */
+	DDRD &= ~(1<<1);	//set pinD1 as input
+	PORTD |= (1<<1); //use pull-up resistor on pinD1
+}
